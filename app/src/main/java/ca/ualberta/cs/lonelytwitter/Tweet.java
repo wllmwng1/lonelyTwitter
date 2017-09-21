ae6dee18f6dc02218f1fa6b198241d1be805c007
wwong1 <wwong1@ualberta.ca>
2017-09-20 18:47:05 -0600

    I/O and GSON added

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -32,6 +32,12 @@ public abstract class Tweet implements Tweettable {
         }
     }
 
+    public void setDate(Date date) {this.date = date; }
+
+    @Override
+    public String toString(){
+        return date.toString() + " | " + message;
+    }
     public Date getDate() {
         return date;
     }
