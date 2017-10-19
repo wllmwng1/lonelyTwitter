27b6ba134b18feda6a68a44312c6413610ad2bb3
wwong1 <wwong1@ualberta.ca>
2017-10-18 20:00:41 -0600

    Lab 7

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -2,17 +2,60 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
 
 import junit.framework.TestCase;
 
 public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
+    private Solo solo;
 
     public LonelyTwitterActivityTest() {
         super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
     }
 
+    public void setUp() throws Exception{
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+    /**
+    * Runs at the beginning of the tests
+    * @throws Exception
+    */
     public void testStart() throws Exception {
         Activity activity = getActivity();
 
+    }
+
+    public void testTweet() {
+        solo.assertCurrentActivity("Wrong Activity", LonelyTwitterActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.body), "Test Tweet!");
+        solo.clickOnButton("Save");
+        solo.enterText((EditText) solo.getView(R.id.body),"");
+
+        assertTrue(solo.waitForText("Test Tweet!"));
+
+        solo.clickOnButton("Clear");
+    }
+
+    public void testClickTweetList() {
+        solo.assertCurrentActivity("Wrong Activity", LonelyTwitterActivity.class);
+        solo.clickOnButton("Clear");
+
+        solo.enterText((EditText) solo.getView(R.id.body), "Test Tweet!");
+        solo.clickOnButton("Save");
+        solo.waitForText("Test Tweet!");
+
+        solo.clickInList(0);
+        solo.assertCurrentActivity("Wrong Activity", EditTweetActivity.class);
+        solo.waitForText("Test Tweet!");
+    }
+     /**
+     * Runs at the end of tests
+     * @throws Exception
+     */
+    @Override
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
     }
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -13,8 +13,10 @@ import java.util.Date;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,8 +26,10 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 public class LonelyTwitterActivity extends Activity {
+	private LonelyTwitterActivity activity = this;
 
 	private static final String FILENAME = "file.sav";
+	public static final String EXTRA_MESSAGE = "MESSAGE";
 	private EditText bodyText;
 	private ListView oldTweetsList;
 	private ArrayList<Tweet> tweetList = new ArrayList<Tweet>();
@@ -38,6 +42,7 @@ public class LonelyTwitterActivity extends Activity {
 
 		bodyText = (EditText) findViewById(R.id.body);
 		Button saveButton = (Button) findViewById(R.id.save);
+        Button clearButton = (Button) findViewById(R.id.clear);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +54,25 @@ public class LonelyTwitterActivity extends Activity {
 				tweetList.add(newTweet);
 				adapter.notifyDataSetChanged();
 				saveInFile();
+			}
+		});
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                setResult(RESULT_OK);
+                tweetList.clear();
+                deleteFile(FILENAME);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+		oldTweetsList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Intent intent = new Intent(activity, EditTweetActivity.class);
+				startActivity(intent);
+				Tweet tweet = (Tweet) oldTweetsList.getItemAtPosition(position);
+				intent.putExtra(EXTRA_MESSAGE, tweet.getMessage());
+				startActivity(intent);
 			}
 		});
 	}
